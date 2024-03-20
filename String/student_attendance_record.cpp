cb833647cd9d0bddb17cf2fcbb0e7eaabfb83190
Hiten Soni <hitensoni58@gmail.com>
2024-03-20 22:07:59 +0530

    Finding if the student is eligible or not

---
+++ String/student_attendance_record.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool checkRecord(string s)
+{
+    int countL = 0;
+    int countA = 0;
+    int maxL = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'A')
+        {
+            countA++;
+        }
+
+        if (s[i] == 'L')
+        {
+            countL++;
+        }
+        else
+        {
+            countL = 0;
+        }
+
+        if (maxL < countL)
+            maxL = countL;
+
+        if (maxL >= 3 || countA >= 2)
+            return false;
+    }
+
+    return true;
+}
+
+int main(){
+    string s = "PALLL";
+    
+    bool ans = checkRecord(s);
+    cout<<ans;
+
+    return 0;
+}
